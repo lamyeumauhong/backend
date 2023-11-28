97718c4b9677899031cb249c6360d816862d908b
lamyeumauhong <duongvu874@gmail.com>
2023-11-28 19:58:35 +0700

    add project

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router()
 const OrderController = require('../controllers/OrderController');
 const { authUserMiddleWare, authMiddleWare } = require("../middleware/authMiddleware");
+
 router.post('/create/:id', authUserMiddleWare, OrderController.createOrder)
 router.get('/get-all-order/:id',authUserMiddleWare, OrderController.getAllOrderDetails)
 router.get('/get-details-order/:id', OrderController.getDetailsOrder)
