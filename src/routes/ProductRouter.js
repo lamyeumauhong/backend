ede8d7073a66e65ee062c464c555c52371af0549
lamyeumauhong <duongvu874@gmail.com>
2023-12-04 23:48:46 +0700

    add project

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router()
 const ProductController = require('../controllers/ProductController');
-const { authMiddleWare } = require("../middleware/authMiddleware");
+const { authUserMiddleWare } = require("../middleware/authMiddleware");
 
-router.post('/create', ProductController.createProduct)
-router.put('/update/:id', authMiddleWare, ProductController.updateProduct)
+router.post('/create',authUserMiddleWare, ProductController.createProduct)
+router.put('/update/:id', authUserMiddleWare, ProductController.updateProduct)
 router.get('/get-details/:id', ProductController.getDetailsProduct)
-router.delete('/delete/:id', authMiddleWare, ProductController.deleteProduct)
+router.delete('/delete/:id', authUserMiddleWare, ProductController.deleteProduct)
 router.get('/get-all', ProductController.getAllProduct)
-router.post('/delete-many', authMiddleWare, ProductController.deleteMany)
+router.post('/delete-many', authUserMiddleWare, ProductController.deleteMany)
 router.get('/get-all-type', ProductController.getAllType)
 
 module.exports = router
