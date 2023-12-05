89484dec91e1fbd70020d7d88fad073e3beaa4c0
Hoàng Nguyên <nguyendzjj@gmail.com>
2023-12-06 00:32:26 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.APP_URL,"http://localhost:3000"]
+}))
+  
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // Thêm extended: true để bảo đảm tương thích với body-parser
 app.use(cookieParser());
